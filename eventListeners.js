004f13e45d25174c316ccf7f2a87cc3889657347
John Perez <tatozan@gmail.com>
2022-09-02 03:01:23 -0300

    proyecto final v4 incorporado eliminacion multiple con checkbox

---
+++ eventListeners.js
@@ -0,0 +1,203 @@
+
+/********************************************************************************************************/
+//Se ejecuta al tocar el boton CALCULAR, es el evento principal del proyecto
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let uuid, tipoOperacion, par, distanciaPorcentajeRecompraReventa, aumentoPorcentajeRecompraReventa, sl, precioMoneda, cantidadMonedas;
+
+    let dataForm = new FormData(event.target);
+
+    tipoOperacion = dataForm.get("operacion");
+    par = dataForm.get("parName");
+    distanciaPorcentajeRecompraReventa = parseFloat(dataForm.get("distanciaPorcentajeRecompraReventaName"));
+    aumentoPorcentajeRecompraReventa = parseFloat(dataForm.get("aumentoPorcentajeRecompraReventaName"));
+    sl = parseFloat(dataForm.get("slName"));
+    precioMoneda = parseFloat(dataForm.get("precioMonedaName"));
+    cantidadMonedas = parseFloat(dataForm.get("cantidadMonedasName"));
+
+    if((distanciaPorcentajeRecompraReventa && aumentoPorcentajeRecompraReventa && sl && precioMoneda && cantidadMonedas) > 0){
+
+        const operacionesIniciales = comprobarLocalStorage("operacionesIniciales");
+
+        //contiene las operaciones, de recompra o reventa, incluida compra/venta incial
+        const operaciones = [];
+        //contiene todas las operaciones promediadas
+        const operacionesProm = [];
+
+        //devuelve fecha y hora de la forma por ejemplo 7/2/2021, 2:05:07 PM. Se utiliza para la hora de cada operacion
+        const date = new Date().toLocaleString();
+
+        const divDatosCompraInicial = document.getElementById("divDatosCompraInicial");
+        divDatosCompraInicial.innerHTML ="";
+
+        const gridOperaciones = document.getElementById("gridOperaciones");
+        gridOperaciones.innerHTML = "";
+
+        //genero un id unico
+        uuid = self.crypto.randomUUID();
+
+        //creo el objeto inicial, con los datos que me ingresaron en el html
+        const operacion0 = new Operacion(uuid, 0, tipoOperacion, par, distanciaPorcentajeRecompraReventa, aumentoPorcentajeRecompraReventa, sl, precioMoneda, cantidadMonedas, date);
+        operaciones.push(operacion0);
+        operaciones[0].calcularMontoInvertido(precioMoneda, cantidadMonedas);
+
+        let precioMonedaEnSl = 0;
+        let porcentajeDistanciaSl = 0;
+        let i = 0;
+        let pnl = 0;
+
+        if(operaciones[0].tipoOperacion === "short" || operaciones[0].tipoOperacion === "long"){
+
+            do{
+                i += 1; 
+
+                //creo una variable nroOperacion y nroOperacionAnterior para que sea mas legible
+                let nroOperacion = i;
+                let nroOperacionAnterior = i-1;
+                let nroOperacionProm = i;
+
+                //datos Recompras
+                let precioMoneda = calcularPrecioMoneda(operaciones[nroOperacionAnterior].precioMoneda, operaciones[nroOperacionAnterior].distanciaPorcentajeRecompraReventa, operaciones[0].tipoOperacion);
+                let cantidadMonedas = calcularCantidadMonedas(operaciones[nroOperacionAnterior].cantidadMonedas, operaciones[nroOperacionAnterior].aumentoPorcentajeRecompraReventa);
+                let inversion = calcularInversion(precioMoneda, cantidadMonedas);
+
+                //Datos operaciones Promediadas
+                let cantidadMonedasProm;
+                let precioMonedaProm;
+                let inversionProm;
+                
+                if(i === 1){
+                    pnl = calcularPnL(operaciones[0].cantidadMonedas, operaciones[nroOperacionAnterior].precioMoneda, precioMoneda, operaciones[0].tipoOperacion);
+                } else {
+                    nroOperacionProm = nroOperacionAnterior - 1;
+                    pnl = calcularPnL(operacionesProm[nroOperacionProm].cantidadMonedas, operacionesProm[nroOperacionProm].precioMoneda, precioMoneda, operaciones[0].tipoOperacion);
+                }
+
+                if(pnl <= operaciones[0].sl){
+
+                    uuid = self.crypto.randomUUID();
+
+                    const operacion = new Operacion(uuid, nroOperacion, operaciones[0].tipoOperacion, par, operaciones[0].distanciaPorcentajeRecompraReventa, operaciones[0].aumentoPorcentajeRecompraReventa, operaciones[0].sl, precioMoneda, cantidadMonedas, date, inversion);    
+                    operaciones.push(operacion);
+
+                    if(i === 1){
+                        precioMonedaProm = (operaciones[nroOperacionAnterior].montoInvertido + operaciones[nroOperacion].montoInvertido) / (operaciones[nroOperacionAnterior].cantidadMonedas + operaciones[nroOperacion].cantidadMonedas);
+                        cantidadMonedasProm = operaciones[nroOperacionAnterior].cantidadMonedas + operaciones[nroOperacion].cantidadMonedas;
+
+                    } else{
+                        precioMonedaProm = (operacionesProm[nroOperacionProm].montoInvertido + operaciones[nroOperacion].montoInvertido) / (operacionesProm[nroOperacionProm].cantidadMonedas + operaciones[nroOperacion].cantidadMonedas);
+                        cantidadMonedasProm = operacionesProm[nroOperacionProm].cantidadMonedas + operaciones[nroOperacion].cantidadMonedas;
+                    }
+
+                    inversionProm = calcularInversion(precioMonedaProm, cantidadMonedasProm);
+
+                    uuid = self.crypto.randomUUID();
+
+                    const operacionProm = new Operacion(uuid, nroOperacion, operaciones[0].tipoOperacion, par, operaciones[0].distanciaPorcentajeRecompraReventa, operaciones[0].aumentoPorcentajeRecompraReventa, operaciones[0].sl, precioMonedaProm, cantidadMonedasProm, date, inversionProm);
+                    operacionesProm.push(operacionProm);
+                }
+            //La calculadora no permite mas que calcular 8 recompras, mas recompras, no se recomienda          
+            } while(pnl <= operaciones[0].sl && i < 8);      
+            
+            //Si i = 1 quiere decir que pnl excede el sl, por lo tanto no podemos hacer ni una recompra
+            if(i === 1){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'SL insuficiente',
+                    text: 'Aumente SL, no puede realizarse ninguna recompra.',
+                    confirmButtonColor: '#98a0ff',
+                  });
+
+            } else {
+
+                operacionesIniciales.push(operacion0);
+            
+                almacenarLocalStorage("operacionesIniciales", operacionesIniciales);
+
+                //extraigo la ultima operacion del arreglo
+                let operacionPromUltima = operacionesProm.length - 1;
+
+                //formula calculo precio de moneda cuando toca SL que elegi como dato de entrada en USDT.
+                precioMonedaEnSl = calcularPrecioMonedaEnSl(operacionesProm[operacionPromUltima].precioMoneda, operacionesProm[operacionPromUltima].cantidadMonedas, operaciones[0].sl, operaciones[0].tipoOperacion);
+
+                //formula calculo distancia de mi operacion #0 al monto de SL que quiero perder, en porcentaje
+                porcentajeDistanciaSl = calcularPorcentajeDistanciaSl(precioMonedaEnSl, operaciones[0].precioMoneda, operaciones[0].tipoOperacion);
+
+                mostrarOperaciones(operaciones, gridOperaciones);
+                mostrarDatosFinales(porcentajeDistanciaSl, precioMonedaEnSl, operacionesProm[operacionPromUltima].cantidadMonedas, operacionesProm[operacionPromUltima].montoInvertido);
+                operaciones[0].mostrarDatosOperacionInicial();
+
+                crearCardsHistorialOperaciones();
+
+                form.reset();
+            }
+        }
+    }
+});
+
+
+/********************************************************************************************************/
+//Evento que cambia el color del boton CALCULAR segun vaya a short o long
+tipoOperacion.addEventListener("click", () => {
+    cambiarColorBoton(tipoOperacion.value, botonCalcular);
+});
+
+
+/********************************************************************************************************/
+//Cada vez que doy al boton reset, se cambia el color del boton calcular por defecto
+botonReset.addEventListener("click", () => {
+    tipoOperacion.value="";
+    cambiarColorBoton(tipoOperacion.value, botonCalcular);
+
+});
+
+
+/********************************************************************************************************/
+//Elimino del dom, array y localStorage las operaciones que selecciono con checkboxs
+botonEliminarOperacion.addEventListener("click", () => {
+    const operacionesIniciales = comprobarLocalStorage("operacionesIniciales");
+
+    listaEliminar.forEach((operacion, indice) => {
+        document.getElementById(`operacion${indice}`).remove();
+    });
+
+    const operacionesFiltradas = operacionesIniciales.filter( (operacion, indice) => !listaEliminar.includes(indice.toString()));
+
+    almacenarLocalStorage("operacionesIniciales", operacionesFiltradas);
+
+    botonEliminarOperacion.style.visibility = "hidden";
+    botonDeseleccionarOperacion.style.visibility = "hidden";
+    
+    listaEliminar = [];
+
+    crearCardsHistorialOperaciones();
+});
+
+
+/********************************************************************************************************/
+//Muestra historial de operaciones al presionar boton Historial operaciones
+let primerClick = true;
+botonMostrarOperacionesIniciales.addEventListener("click", () => {
+    if (primerClick == true){
+        primerClick = false;
+        botonMostrarOperacionesIniciales.style.background="#5fb6ff";
+    
+        crearCardsHistorialOperaciones();
+    } else {
+        primerClick = true;
+        botonMostrarOperacionesIniciales.style.background="#6c757d";
+        divOperacionesIniciales.innerHTML = "";
+    }
+});
+
+botonDeseleccionarOperacion.addEventListener("click", () => {
+    listaEliminar.forEach(indice => {
+        let checkBox = document.getElementById(`operacion${indice}`).firstElementChild.firstElementChild;
+        checkBox.checked = false;
+        botonDeseleccionarOperacion.style.visibility = "hidden";
+        botonEliminarOperacion.style.visibility = "hidden";
+        listaEliminar = [];
+    });
+});
+
---
+++ main.js
@@ -0,0 +1,360 @@
+//activo popovers en todo el documento
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
+
+
+/*-------------------------------------------CLASES-------------------------------------------*/
+//declaro objeto operacion
+class Operacion{
+    //constructor de la clase
+    constructor(id, numeroOperacion = 0, tipoOperacion, par = "predeterminado", distanciaPorcentajeRecompraReventa = 0, aumentoPorcentajeRecompraReventa = 0, sl, precioMoneda = 0, cantidadMonedas = 0, date="indeterminado", montoInvertido = 0){
+        this.id = id;
+        this.numeroOperacion = numeroOperacion;
+        this.tipoOperacion = tipoOperacion;
+        this.par = par
+        this.distanciaPorcentajeRecompraReventa = distanciaPorcentajeRecompraReventa;
+        this.aumentoPorcentajeRecompraReventa = aumentoPorcentajeRecompraReventa;
+        this.sl = sl;
+        this.precioMoneda = precioMoneda;
+        this.cantidadMonedas = cantidadMonedas;
+        this.montoInvertido = montoInvertido;
+        this.date = date;
+    }
+
+    //metodos de la clase
+    calcularMontoInvertido(precioMoneda, cantidadMonedas){
+        this.montoInvertido = precioMoneda * cantidadMonedas;
+    }
+
+    mostrarDatosOperacionInicial(){
+        const divDatosCompraInicial = document.getElementById('divDatosCompraInicial');
+        divDatosCompraInicial.style.border = "1px solid #ddd";
+        divDatosCompraInicial.innerHTML = "";
+
+        divDatosCompraInicial.innerHTML += `
+            <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Datos Operación Inicial
+                </button>
+            </h2>
+
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample" style="">
+                <div class="accordion-body">
+                        <p> <span class="itemInfo"> Moneda: </span>${this.par} </p>
+                        <p> <span class="itemInfo"> Precio de compra: </span>$${this.precioMoneda.toFixed(3)} </p>
+                        <p> <span class="itemInfo"> Monto invertido en dólares: </span> $${this.montoInvertido.toFixed(3)} </p>
+                        <p> <span class="itemInfo"> Tamaño compra: </span> ${this.cantidadMonedas.toFixed(2)} monedas </p>
+                        <p> <span class="itemInfo"> Tipo de operación: </span> ${this.tipoOperacion} </p>
+                </div>
+            </div>
+        `;
+    }
+}
+
+
+/*---------------------------------------------FUNCIONES---------------------------------------------*/
+
+
+/**************************************almacenarLocalStorage*************************************/
+//Almacenar array de objetos en localStorage
+function almacenarLocalStorage(index, objectsArray){
+    localStorage.setItem(index, JSON.stringify(objectsArray));
+}
+
+
+/***************************************comprobarLocalStorage*****************************************/
+//comprobar si existe un array de objetos en un indice determinado, si existe lo devuelve, sino, crea un array de objetos vacio
+function comprobarLocalStorage(index){
+    //compruebo si esta creado mi localStorage, si no esta creado lo creo, y si esta, le envio las operaciones que tenia previamente
+    return JSON.parse(localStorage.getItem(index)) ?? [];
+}
+
+
+/**************************************obtenerListadoMonedas********************************************/
+//obtengo las monedas de la api de futuros de Binance
+const obtenerListadoMonedas = async() => {
+    try{
+        const respuesta = await axios.get("https://www.binance.com/fapi/v1/exchangeInfo");
+
+        par.innerHTML = "";
+
+        par.innerHTML += `
+            <option selected disabled value=""> SELECT </option>
+        `
+        respuesta.data.symbols.forEach((moneda) => {
+            par.innerHTML += `
+                <option value="${moneda.pair}">${moneda.pair}</option>
+            `
+        });
+    }
+    catch(error){
+        console.log(error);
+    }
+} 
+
+
+/******************************************mostrarOperaciones***************************************/
+//Mostrar operaciones de recompra / reventa
+function mostrarOperaciones(operaciones, gridOperaciones){
+    let codigoHTMLTabla;
+    gridOperaciones.innerHTML = "";
+
+    codigoHTMLTabla = `
+    <table class="table table-hover animate__animated animate__fadeIn">
+        <thead>
+            <tr>
+                <th scope="col"> # </th>
+                <th scope="col"> PRECIO </th>
+                <th scope="col"> MONEDAS </th>
+                <th scope="col"> USDT </th>
+            </tr>
+        </thead>
+        <tbody>
+    `;
+
+    operaciones.forEach((operacion, indice) => {
+
+        //no muestro operacion inicial, solo las recompras / reventas que es lo que me interesa para operar
+        if(indice != 0){
+            if(indice % 2 == 0){
+                codigoHTMLTabla += `
+                    <tr class="table-light">
+                        <th scope="row"> ${operacion.numeroOperacion} </th>
+                        <td>$${operacion.precioMoneda.toFixed(3)}</td>
+                        <td>${operacion.cantidadMonedas.toFixed(3)}</td>
+                        <td>$${operacion.montoInvertido.toFixed(2)}</td>
+                    </tr>  
+                `
+
+            } else {
+                codigoHTMLTabla += `
+                    <tr>
+                        <th scope="row"> ${operacion.numeroOperacion} </th>
+                        <td>$${operacion.precioMoneda.toFixed(3)}</td>
+                        <td>${operacion.cantidadMonedas.toFixed(3)}</td>
+                        <td>$${operacion.montoInvertido.toFixed(2)}</td>
+                    </tr>  
+                ` 
+            }
+        }
+    });
+
+    codigoHTMLTabla += `
+            </tbody>
+        </table>
+    `
+    gridOperaciones.innerHTML = codigoHTMLTabla;
+}
+
+
+/******************************************mostrarDatosFinales*****************************************/
+//Crea un acordion con datos adicionales al calculo de las operaciones
+function mostrarDatosFinales(porcentajeDistanciaSl, precioMonedaEnSl, cantidadMonedas, montoInvertido){
+    const divDatosFinales = document.getElementById('divDatosFinales');
+    divDatosFinales.style.border = "1px solid #ddd";
+    divDatosFinales.innerHTML = "";
+
+    divDatosFinales.innerHTML += `
+        <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Datos Adicionales
+            </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample" style="">
+            <div class="accordion-body">
+                <p> <span class="itemInfo"> SL(${porcentajeDistanciaSl.toFixed(2)}%) </span></p>
+                <p> Precio moneda al tocar SL: <span class="itemInfo">$${precioMonedaEnSl.toFixed(3)} </span></p>
+                <p> Cantidad de monedas compradas utilizando todas las recompras: <span class="itemInfo">${cantidadMonedas.toFixed(3)} </span> </p>
+                <p> Monto total invertido utilizando todas las recompras: <span class="itemInfo"> $${montoInvertido.toFixed(3)} </span> </p>
+            </div>
+        </div>
+    `
+}
+
+
+/****************************************crearHTMLHistorialOperaciones*************************************/
+//Crear HTML de historial de operaciones, dado un div contenedor, y el array de objetos a mostrar
+function crearHTMLHistorialOperaciones(divContenedor, arrayObject){
+    divContenedor.innerHTML = "";
+
+    arrayObject.forEach((operacion, indice) => {
+        divContenedor.innerHTML += `
+            <div class="card bg-light border-light animate__animated animate__fadeIn" id="operacion${indice}">
+                <div class="card-header">${indice + 1}. ${operacion.par}
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                </div>
+                <div class="card-body" id="card-operacion">
+                    <p class="card-text"> <span class="itemInfo"> Tipo operación:  </span> ${operacion.tipoOperacion}  </p>
+                    <p class="card-text"> <span class="itemInfo"> Timestamp:  </span> ${operacion.date}  </p>
+                    <p class="card-text"> <span class="itemInfo"> Precio moneda:   </span> $${operacion.precioMoneda.toFixed(3)} </p>
+                    <p class="card-text"> <span class="itemInfo"> Cantidad monedas:  </span> ${operacion.cantidadMonedas.toFixed(3)}  </p>
+                    <p class="card-text"> <span class="itemInfo"> Monto invertido:  </span> $${operacion.montoInvertido.toFixed(2)}  </p>
+                    <button type="button" class="btn btn-danger"> <i class="fa-solid fa-trash"></i> </button>
+                    <button type="button" class="btn btn-success"> <i class="fa-solid fa-book"></i> </button>
+                </div>
+            </div>
+        `       
+    });
+}
+
+
+/*************************************crearCardsHistorialOperaciones***********************************/
+//Crea el historial de operaciones
+function crearCardsHistorialOperaciones(){
+
+    const operacionesIniciales = comprobarLocalStorage("operacionesIniciales");
+    console.log("operacionesIniciales cuando creo las cards");
+    console.log(operacionesIniciales);
+
+    crearHTMLHistorialOperaciones(divOperacionesIniciales, operacionesIniciales);
+
+    operacionesIniciales.forEach((operacion, indice) => {
+
+        let operacionEliminar = document.getElementById(`operacion${indice}`).lastElementChild.children[5];
+        let operacionInicialDatos = document.getElementById(`operacion${indice}`).lastElementChild.lastElementChild;
+        //let cardOperacion = document.getElementById(`operacion${indice}`);
+        let checkBox = document.getElementById(`operacion${indice}`).firstElementChild.firstElementChild;
+        //Boton cargar datos de operaciones
+        operacionInicialDatos.addEventListener("click", () => {
+
+            cargarOperacionFormulario(
+                document.getElementById("tipoOperacion"), 
+                document.getElementById("par"), 
+                document.getElementById("distanciaPorcentajeRecompraReventa"), 
+                document.getElementById("aumentoPorcentajeRecompraReventa"), 
+                document.getElementById("sl"), 
+                document.getElementById("precioMoneda"), 
+                document.getElementById("cantidadMonedas"), 
+                operacion
+            );
+
+            cambiarColorBoton(tipoOperacion.value, botonCalcular);
+        });
+
+        operacionEliminar.addEventListener("click", () => {
+            Swal.fire({
+                title: 'Desea eliminar la operación?',
+                text: "Luego, no será capaz de recuperarla!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#5fb6ff',
+                cancelButtonColor: '#6c757d',
+                confirmButtonText: 'Ok'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    eliminarOperacion(`operacion${indice}`, indice, operacionesIniciales);
+
+                    crearCardsHistorialOperaciones();
+                    Swal.fire({
+                        title: 'Borrada!',
+                        text:"La operación ha sido eliminada.",
+                        icon: 'success',
+                        confirmButtonColor: '#5fb6ff',
+                        confirmButtonText: 'Ok'
+                    });
+                }
+            });
+        });
+
+        checkBox.addEventListener("click", () => {
+            comprobarOperacionesAEliminar(checkBox, listaEliminar, indice);    
+        });
+    });
+}
+
+
+/***********************************cargarOperacionFormulario*************************************/
+//Carga operaciones en el formulario, para luego poder recalcular los datos de esa operacion
+function cargarOperacionFormulario(tipoOperacion, par, distanciaPorcentajeRecompraReventa, aumentoPorcentajeRecompraReventa, sl, precioMoneda, cantidadMonedas, operationObject){
+    tipoOperacion.value = operationObject.tipoOperacion;
+    par.value = operationObject.par;
+    distanciaPorcentajeRecompraReventa.value = operationObject.distanciaPorcentajeRecompraReventa;
+    aumentoPorcentajeRecompraReventa.value = operationObject.aumentoPorcentajeRecompraReventa;
+    sl.value = operationObject.sl;
+    precioMoneda.value = operationObject.precioMoneda;
+    cantidadMonedas.value = operationObject.cantidadMonedas;
+}
+
+
+/**********************************comprobarOperacionesAEliminar***************************************/
+//Comprueba las operaciones seleccionadas mediante los checkboxs, para hacer multiple eliminacion
+function comprobarOperacionesAEliminar(checkBox, listaEliminar, indice){
+    if(checkBox.checked === true){
+        botonEliminarOperacion.style.visibility = "visible";
+        listaEliminar.push(`${indice}`);
+    } else if(checkBox.checked === false){
+        let indiceElementoAEliminar = listaEliminar.indexOf(`${indice}`);
+        //se compara con -1 porque es una condicion de error del metodo indexOf, por si no encuentra el elemento buscado
+        if(indice != -1){ 
+            listaEliminar.splice(indiceElementoAEliminar, 1);
+        }
+    }
+
+    if(listaEliminar.length == 0){
+        botonEliminarOperacion.style.visibility = "hidden";
+        botonDeseleccionarOperacion.style.visibility = "hidden";
+    }else if(listaEliminar.length > 0){
+        botonDeseleccionarOperacion.style.visibility = "visible";
+    }
+}
+
+
+/***************************************eliminarOperacion***********************************/
+//Elimina operacion del dom, del array y finalmente del localStorage
+function eliminarOperacion(elementoDOM, indice, arrayObject){
+    document.getElementById(elementoDOM).remove();
+
+    arrayObject.splice(indice, 1);
+
+    almacenarLocalStorage("operacionesIniciales", arrayObject);
+}
+
+
+/*************************************cambiarColorBoton*****************************************/
+//Cambia el color del boton CALCULAR dependiendo del tipo de operacion
+function cambiarColorBoton(tipoOperacion, boton){
+    switch (tipoOperacion) {
+        case "short":
+            boton.style.background = "rgb(246, 70, 93)";
+            break;
+        case "long":
+            boton.style.background = "rgb(14, 203, 129)";
+            break;
+        default:
+            boton.style.background = "#6c757d";
+            break;
+    }
+}
+
+
+
+/*-------------------------------------------CODIGO-------------------------------------------*/
+
+//obtencion elementos del dom
+const operacionesIniciales = comprobarLocalStorage("operacionesIniciales");
+
+console.log("operaciones iniciales en el hilo principal");
+console.log(operacionesIniciales);
+
+const form = document.getElementById('idForm');
+const botonMostrarOperacionesIniciales = document.getElementById("idBtnDropDown");
+const divOperacionesIniciales = document.getElementById("divOperacionesIniciales");
+const gridOperaciones = document.getElementById("gridOperaciones");
+const tipoOperacion = document.getElementById("tipoOperacion");
+const botonCalcular = document.getElementById("botonCalcular");
+const botonReset = document.getElementById("botonReset");
+const botonEliminarOperacion = document.getElementById("botonEliminarOperacion");
+const botonDeseleccionarOperacion = document.getElementById("botonDeseleccionarOperacion");
+
+let listaEliminar = [];
+
+//obtengo listado de monedas de Binance Futures
+obtenerListadoMonedas();
+
+
+
+
+/*let uuid = self.crypto.randomUUID();*/
+
